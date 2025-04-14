3973190629b086f41de3e88bf7bd95236d451212
sramos <sramos@student.codam.nl>
2025-04-14 15:18:49 +0200

    testing sara

--- TESTING/http_tcpServer_linux.cpp
+++ TESTING/http_tcpServer_linux.cpp
@@ -1,4 +1,13 @@
-#include "http_tcpServer_linux.h"
+#include "http_tcpServer_linux.hpp"
+
+void	log(const std::string &message){
+	std::cout << message <<  std::endl;
+}
+
+void exitWithError(const std::string &errorMessage){
+	log("ERROR " + errorMessage);
+	exit (1);
+}
 
 TcpServer::TcpServer(std::string ip_address, int port)
 	: m_ip_address(ip_address), m_port(port), m_socket(),
@@ -6,6 +15,9 @@ TcpServer::TcpServer(std::string ip_address, int port)
 	m_socketAddress_len(sizeof(m_socketAddress)),
 	m_serverMessage(buildResponse())
 {
+	m_socketAddress.sin_family = AF_INET;
+	m_socketAddress.sin_port = htons(m_port);
+	m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
 	startServer();
 }
 
@@ -36,19 +48,57 @@ void TcpServer::startListen(){
 		<< " PORT: " << ntohs(m_socketAddress.sin_port)
 		<< " ***\n\n";
 	log(ss.str());
+
+	int bytesReceived;
+
+	while (true){
+		log("======= Waiting for a new connection =======\n\n\n");
+		acceptConnection(m_new_socket);
+		//epoll();
+		char buffer[BUFFER_SIZE] = {0};
+		bytesReceived = read(m_new_socket, buffer, BUFFER_SIZE);
+		if (bytesReceived < 0)
+			exitWithError("Failed to read bytes from client socket connection");
+		std::ostringstream ss;
+		ss << " -------- Received Request from client -------\n\n";
+		log(ss.str());
+
+		sendResponse();
+
+		close(m_new_socket);
+	}
+}
+
+void TcpServer::acceptConnection(int &new_socket){
+	new_socket = accept(m_socket, (sockaddr *)&m_socketAddress, &m_socketAddress_len);
+	if (new_socket < 0){
+		std::ostringstream ss;
+		ss << "Server failed to accept incoming from ADDRESS: "
+		<< inet_ntoa(m_socketAddress.sin_addr) << "; PORT: "
+		<< ntohs(m_socketAddress.sin_port);
+		exitWithError(ss.str());
+	}
+}
+
+std::string TcpServer::buildResponse(){
+	std::string htmlFile = "<!DOCTYPE html><html lang=\"en\"><body><h1> HOME </h1><p> Hello from your Serber :) </p></body></html>";
+	std::ostringstream ss;
+	ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << htmlFile.size() << "\n\n" << htmlFile;
+	return ss.str();
+}
+
+void TcpServer::sendResponse(){
+	long bytesSent;
+
+	bytesSent = write(m_new_socket, m_serverMessage.c_str(), m_serverMessage.size());
+	if (bytesSent == m_serverMessage.size())
+		log("------ Server Response sent to client -------\n\n");
+	else
+		log("Error sending response to client");
 }
 
 void TcpServer::closeServer(){
 	close(m_socket);
-	close(m_new_socket); //Do not understand this new_socket.
+	close(m_new_socket);
 	exit (0);
-}
-
-void	log(const std::string &message){
-	std::cout << message <<  std::endl;
-}
-
-void exitWithError(const std::string &errorMessage){
-	log("ERROR " + errorMessage);
-	exit (1);
 }
--- TESTING/server_linux.cpp
+++ TESTING/server_linux.cpp
@@ -1,6 +1,7 @@
-#include "http_tcpServer_linux.h"
+#include "http_tcpServer_linux.hpp"
 
 int	main(){
-	TcpServer server = TcpServer("0.0.0.0", 8080);
+	TcpServer server = TcpServer("127.0.0.1", 8080);
+	server.startListen();
 	return 0;
 }
